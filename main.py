8dad5c75bd56e21308b0c683915de65ab1536083
Samuel Pimenta <samuel.pimenta@athenasolucoes.com>
2025-01-17 14:15:28 -0300

    Added a function to verify if all fields are in the csv file

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 
 from src.archer_formatter.read_csv import read_file
-from src.archer_formatter.sanitize_csv import sanitize_file
 from src.archer_formatter.convert_to_xml import convert_file
 from src.archer_formatter.apply_rules import create_json_handler
+from src.archer_formatter.sanitize_csv import check_mandatory_fields
+
 
 #----------------------------------------------------------------#
 #                                                                #
 #     Desc: Script para converter arquivos CSV                   #
 #     em XML mantendo o padrão elencado pelo BACEN CADOC 5050    #                                                  
-#     Author: Samuel Pimenta & Pedro Brito                       #                                        
+#     Author: Samuel Pimenta                                     #                                        
 #     Company: Athena Soluções Inteligentes                      #
 #     Data: 01/10/24                                             #
 #                                                                #
@@ -18,6 +19,31 @@ from src.archer_formatter.apply_rules import create_json_handler
 
 if __name__ == "__main__":
 
-    df = read_file("data/base.csv")
-    convert_file(df)
-    create_json_handler(df)
+    class Taskflow:
+        def __init__(self, csv_path):
+            self.csv_path = csv_path
+            self.df = None
+
+        def execute(self):
+            # Reading the csv file
+            print("Reading the csv file...")
+            self.df = read_file(self.csv_path)
+
+            # Creating the json handler
+            print("Creating the json handler...")
+            create_json_handler(self.df)
+
+            # Checking the mandatory fields
+            print("Checking the mandatory fields...")
+            if not check_mandatory_fields(self.df):
+                print("There is missing mandatory fields in the csv file... Exiting")
+                return  # Interrompe o fluxo se faltarem campos obrigatórios
+
+            # Converting the file to xml
+            print("Converting the file to xml...")
+            convert_file(self.df)
+            print("The operation has been completed successfully!")
+
+
+    flow = Taskflow("data/base.csv")
+    flow.execute()
--- src/archer_formatter/sanitize_csv.py
+++ src/archer_formatter/sanitize_csv.py
@@ -1,4 +1,33 @@
 import pandas as pd
 
-def sanitize_file(csv_path):
-    pass
+mandatory_fields = ['codigoDocumento',
+               'dataBase',
+               'codigoConglomerado',
+               'cnpj',
+               'tipoRemessa',
+               'opcaoPorProvisaoAcumulada',
+               'idEvento',
+               'categoriaNivel1',
+               'totalPerdaEfetiva',
+               'totalProvisao',
+               'totalRecuperado',
+               'fonteRecuperacao',
+               'valorTotalRisco',
+               'dataOcorrencia',
+               'categoriaNivel1Consol',
+               'numEventosTotalConsol',
+               'perdaEfetivaTotalConsol',
+               'codigoSistema',
+               'nomeSistema',
+               'codigoConta',
+               'nomeConta']
+
+def check_mandatory_fields(df):
+    for field in mandatory_fields:
+        if field not in df.columns:
+            print("\n ---------------------------- \n")
+            print(f"Field {field} not found in file")
+            print("\n ---------------------------- \n")
+
+            return False
+    return True
