6ee9317477a4f135d82742e50c20cad3e50f570a
flexinzop <190058986@aluno.unb.br>
2025-01-30 11:22:43 -0300

    Removendo arquivos desnecessários

--- src/archer_formatter/field_mapping.py
+++
@@ -1,7 +0,0 @@
-# src/archer_formatter/field_mapping.py
-
-FIELD_MAPPING = {
-    'CSV_Column1': 'XML_Tag1',
-    'CSV_Column2': 'XML_Tag2',
-    # ...
-}
--- src/archer_formatter/utils.py
+++
@@ -1,6 +0,0 @@
-# src/archer_formatter/utils.py
-
-import logging
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
